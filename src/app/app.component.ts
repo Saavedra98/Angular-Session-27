3a63196ee9079809ee064853fb8cd6a74f4a139f
Saavedra98 <saavedraerazoc@gmail.com>
2023-01-26 09:26:23 -0500

    calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'session-27';
+
+  resultado!:number;
+  numero1!:number;
+  numero2!:number;
+
+  elemento = false;
+
+  calcular(operacion:string){
+
+    if(operacion==='1'){
+      this.resultado = this.numero1 + this.numero2;
+    }else if(operacion === '2'){
+      this.resultado = this.numero1 - this.numero2;
+    }else if(operacion === '3'){
+      this.resultado = this.numero1 * this.numero2;
+    }else{
+      this.resultado = this.numero1 / this.numero2;
+    }
+
+    this.elemento = true;
+    
+
+  }
+
+
 }
